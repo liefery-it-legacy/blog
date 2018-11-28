0888f94b71c5bd3a107f505410c5417fcb71524a
irmela <irmela@liefery.com>
2018-11-28 15:23:37 +0100

    [#LIEF-10960] Add more teasers to engineering blog

--- js/layouts/blog.js
+++ js/layouts/blog.js
@@ -1,3 +1,16 @@
-if (window.location.pathname == '/' && window.location.hash == '') {
-  window.location.hash = '#page1';
-}
+const initLinkTo = () => {
+  const dataLinks = document.querySelectorAll(".js--link-to");
+
+  console.log(dataLinks);
+
+  dataLinks.forEach((element) => {
+    const link = element;
+    link.onclick = function (e) {
+      window.location = link.dataset.link;
+    };
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  initLinkTo();
+});
